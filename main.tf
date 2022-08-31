137e32380809aec1f5dae9540838c9d04a168120
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-08-31 17:30:38 +0530

    aa

--- main.tf
+++ main.tf
@@ -2,22 +2,12 @@ provider "aws" {
   region = "ap-south-1"
 }
 variable "vpc_id" {}
-
 data "external" "id" {
   program   = ["bash", "${path.module}/check_vpc_id_script.sh"]
   query = {
     id = var.vpc_id
   }
 }
-
-locals {
-  id_matched = length(data.external.id.result.VpcId) > 0 ? 1 : 0
-}
-
-output "id_matched" {
-  value = local.id_matched
-}
-
 resource "aws_instance" "ec2" {
   ami           = "ami-068cda7597e78094b"
   instance_type = "t2.micro"
@@ -25,5 +15,5 @@ resource "aws_instance" "ec2" {
   tags = {
     "Name" = "sahil-test-ec2"
   }
-  count = local.id_matched
+  count = length(data.external.id.result.VpcId) > 0 ? 1 : 0
 }
